00ac5e41453cae7418e1ce5bf41efbd75875f605
Mudasiru Abdulsalam <108347465+tosvn@users.noreply.github.com>
2023-09-12 16:22:19 -0700

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
--- Creating a table
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT
-	name VARCHAR(256) NOT NULL);
-
+-- creating table
+CREATE TABLE IF NOT EXISTS force_name(
+id INT,
+name VARCHAR(256) NOT NULL);
