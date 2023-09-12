8614c635570a8e68516a7bd379d00955e1ef30ae
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-09-12 16:12:46 -0700

    9-cities

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Listing all the cities contained in the database
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
+
