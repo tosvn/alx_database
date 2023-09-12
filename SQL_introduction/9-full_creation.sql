9c798a6e494154a99a7bdb1beae38a69505ac762
Mudasiru Abdulsalam <108347465+tosvn@users.noreply.github.com>
2023-09-12 03:48:11 -0700

    Update 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,12 @@
 -- Creating a table
-CREATE TABLE IF EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT
 	name VARCHAR(256)
 	score INT
 );
+
+INSERT INTO second_table (id, name, scores) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
