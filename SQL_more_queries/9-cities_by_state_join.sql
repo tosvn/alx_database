2e493f7e2c961cfa65842a41a78e75eb2eeb8757
Mudasiru Abdulsalam <108347465+tosvn@users.noreply.github.com>
2023-09-12 16:17:38 -0700

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Listing all the cities contained in the database
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name 
 FROM cities
 INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
